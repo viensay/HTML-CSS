a22bcd2803d0799544ad922aa40a22ea0db0ff50
Viensay <viensay@gmail.com>
2024-09-24 13:17:27 +0200

    init

---
+++ source/tableau.php
@@ -0,0 +1,52 @@
+<?php
+    // $note = [18,13,5,9,10];
+    // $moyenne = ($note[0] + $note[1] + $note[2] + $note[3] + $note[4])/5;
+    // $somme = $note[0] + $note[1] + $note[2] + $note[3] + $note[4];
+    // echo 'La somme est :' . $somme . "\n" ;
+    // echo 'La moyenne est : ' . $moyenne . "\n";
+
+    // $animal = ["chien", "chat", "éléphant", "tortue"];
+    // echo 'Le deuxième élément est ' . $animal[1] . ' et le quatrième est ' . $animal[3];
+    // echo "\n";
+
+    // $semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+    // echo 'On est ' . $semaine[2] . "\n";
+    // echo 'Avant hier on était ' . $semaine[0];
+    // echo ' et après demain on sera ' . $semaine[4] . '.';
+
+    // $tab2dim = ["James", "Bond", "M", "7/7/2007",["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"],"London"];
+    // echo 'Prénom :' . $tab2dim[0] . "<br>Nom : " . $tab2dim[1] . "<br>" ;
+
+    // $users = [  'firstname' => 'Lara',
+    //             'lastname' => 'Croft',
+    //             'gender' => 'F',
+    //             'dateOfBirth' => '23/10/1995',
+    //             'notes' => [18,13,5,10,9],
+    //             'city' => 'London'  ];
+    // // echo $users['city'];
+
+    // $users['lastname'] = 'Elric';
+    // $users['notes'][4] = 17;
+
+    // echo 'Nom : ' . $users['lastname'] . "\n" .
+    //     'Prénom : ' . $users['firstname'] . "\n" .
+    //     'Note : ' . $users['notes'][4] . "\n";
+
+    // $user2 = [
+    //     'firstname' => 'Harry',
+    //     'lastname' => 'Potter',
+    //     'gender' => 'M',
+    //     'dateOfBirth' => '30/08/1981',
+    //     'notes' => [15,20,5,3,18],
+    //     'City' => 'Private Drive',
+    //     'joursDeSemaine' => ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+    // ];
+
+    // echo 'Nom : ' . $user2['lastname'] . "\n".
+    //         'Prénom : ' . $user2['firstname'] . "\n" .
+    //         'Moyenne : ' . ($user2['notes'][0] + $user2['notes'][1]+ $user2['notes'][2]+ $user2['notes'][3]+ $user2['notes'][4])/5 . "\n" .
+    //         'Demain on sera : ' . $user2['joursDeSemaine'][3];
+
+    // $user2['joursDeSemaine'][7]= "Néant";
+
+?>
